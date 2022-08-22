bf04041f2a14b6e06153e8e7698c557e984a2f68
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-08-22 18:56:03 -0500

    scheduler

---
+++ Develop/test.js
@@ -0,0 +1,12 @@
+var time = [0,1,2,,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+
+for (i=0; i<time.length; i++){
+
+console.log (time[i])}
+
+var color = ['Red','Orange','Yellow','Green','Blue','Indigo','Violet'];
+
+for (i=0; i<color.length; i++){
+
+    console.log("index"+ i + " = " + color[i])
+}
